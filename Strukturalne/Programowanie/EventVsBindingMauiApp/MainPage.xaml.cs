db6a0f5c9b8d85f2698166a66faab1fd0dded123
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-09 11:10:27 +0200

    koniec lekcji jakies projekty nowe i kotki

---
+++ Strukturalne/Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace EventVsBindingMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (rotationLabel is not null && sender is Slider slider)
+            {
+                rotationLabel.Rotation = slider.Value;
+                rotationLabel.Text = slider.Value.ToString();
+            }
+        }
+    }
+}
---
+++ Strukturalne/Programowanie/SimpleCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //pobrać dane wejsciowe
+            //var firstNumber = int.Parse(firstNumberEntry.text)
+            //int firstNumber;
+            if (int.TryParse(firstNumberEntry.Text, out int firstNumber)
+                && int.TryParse(secondNumberEntry.Text, out int secondNumber))
+            {
+                resultLabel.Text = $"Wynik dodawania to {firstNumber + secondNumber}";
+                resultLabel.BackgroundColor = Colors.Green;
+            }
+            else
+            {
+                resultLabel.Text = $"Podales nieprawidlowe dane.";
+                resultLabel.BackgroundColor = Colors.Red;
+            }
+        }
+    }
+}
