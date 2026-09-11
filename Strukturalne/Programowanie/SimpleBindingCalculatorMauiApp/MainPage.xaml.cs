f0edd8a5316ed99f7104d0366ed2377e27db62d5
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-11 11:40:05 +0200

    koniec lekcji

---
+++ Strukturalne/Programowanie/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace SimpleBindingCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public string Addition { get; set; }
+
+        private string returnAddition { get; set; }
+
+        public string ReturnAddition
+        {
+            get { return returnAddition; }
+            set
+            {
+                returnAddition = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //pobrać dane wejsciowe
+            //var firstNumber = int.Parse(firstNumberEntry.text)
+            //int firstNumber;
+            /*if (int.TryParse(firstNumberEntry.Text, out int firstNumber)
+                && int.TryParse(secondNumberEntry.Text, out int secondNumber))
+            {
+                resultLabel.Text = $"Wynik dodawania to {firstNumber + secondNumber}";
+                resultLabel.BackgroundColor = Colors.Green;
+            }
+            else
+            {
+                resultLabel.Text = $"Podales nieprawidlowe dane.";
+                resultLabel.BackgroundColor = Colors.Red;
+            }*/
+
+            if (int.TryParse(firstNumberEntry.Text, out int firstNumber)
+                && int.TryParse(secondNumberEntry.Text, out int secondNumber))
+            {
+                resultLabel.Text = $"Wynik dodawania to {firstNumber + secondNumber}";
+                resultLabel.BackgroundColor = Colors.Green;
+            }
+            else
+            {
+                resultLabel.Text = $"Podales nieprawidlowe dane.";
+                resultLabel.BackgroundColor = Colors.Red;
+            }
+
+        }
+    }
+}
